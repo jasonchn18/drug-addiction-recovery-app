04bee5cb8f07e265ec41723ddafa0a18f042ad17
jasonchn18 <jasonchn18@gmail.com>
2022-02-18 18:11:18 +0800

    #18 - Firestore Streams

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp_app/services/auth.dart';
+import 'package:fyp_app/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:fyp_app/screens/brew_list.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -15,51 +19,56 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(240,240,235,1.0),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton.icon(
-                      onPressed: () {
-                        // Navigator.pushNamed(context, '/home');
-                      }, 
-                      icon: Icon(
-                        Icons.home_rounded,
-                        color: Colors.black,
-                      ),
-                      label: Text(
-                        'Home screen',
-                        style: TextStyle(
+    return StreamProvider<QuerySnapshot?>.value(
+      value: DatabaseService(uid:'').brews,
+      initialData: null,
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(240,240,235,1.0),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              BrewList(),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: TextButton.icon(
+                        onPressed: () {
+                          // Navigator.pushNamed(context, '/home');
+                        }, 
+                        icon: Icon(
+                          Icons.home_rounded,
                           color: Colors.black,
+                        ),
+                        label: Text(
+                          'Home screen',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right:8.0),
-                      child: TextButton.icon(
-                        onPressed: () async {
-                          await _auth.signOut();
-                        }, 
-                        icon: Icon(Icons.person), 
-                        label: Text('Logout')
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right:8.0),
+                        child: TextButton.icon(
+                          onPressed: () async {
+                            await _auth.signOut();
+                          }, 
+                          icon: Icon(Icons.person), 
+                          label: Text('Logout')
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
