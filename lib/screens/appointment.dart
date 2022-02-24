959ac715a4ff28dc85ed5e8cbf808259cf58d795
jasonchn18 <jasonchn18@gmail.com>
2022-02-25 05:27:59 +0800

    UI for Appointment Booking

--- lib/screens/appointment.dart
+++ lib/screens/appointment.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:im_stepper/stepper.dart';
 // import 'dart:async';
 
 class Appointment extends StatefulWidget {
@@ -11,36 +12,86 @@ class Appointment extends StatefulWidget {
 }
 
 class _AppointmentState extends State<Appointment> {
+  int activeStep = 0;
+  int upperBound = 2;
+
+  void _prevButtonAction() {
+      setState(() {
+        activeStep--;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(240,240,235,1.0),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // align column's children to the left
-          children: <Widget>[
-            TextButton.icon(
-              onPressed: () {}, 
-              icon: Icon(
-                Icons.calendar_today_rounded,
-                color: Colors.black,
-              ),
-              label: Text(
-                'Appointment screen',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: Color.fromRGBO(240,240,235,1.0),
+        body: Column(
+          children: [
+            SizedBox(height: 10),
+            NumberStepper(
+              activeStep: activeStep,  // this is the default actually
+              numbers: [1,2,3],
+              direction: Axis.horizontal,
+              enableNextPreviousButtons: false,
+              enableStepTapping: false,
+              stepColor: Color.fromRGBO(134,148,133,1),
+              activeStepColor: Color.fromRGBO(4, 98, 126,1.0),
+              numberStyle: TextStyle(color: Colors.white),
+              onStepReached:(index) {
+                setState(() {
+                  activeStep = index;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            header(),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: body(),
               ),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/article');
-              },
-              child: Text(
-                'Sample Article',
-                style: TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                  color: Colors.black,
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(width: 30),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: activeStep > 0 ? ()=>_prevButtonAction() : null,
+                          child: Text('Previous'),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(4, 98, 126, 0.7)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 50),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (activeStep < upperBound) {
+                              setState(() {
+                                activeStep++;
+                              });
+                            }
+                          },
+                          child: activeStep < upperBound ? Text('Next') : Text('Submit'),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(4, 98, 126, 0.7)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 30),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -49,15 +100,120 @@ class _AppointmentState extends State<Appointment> {
       ),
     );
   }
+
+  // Returns the header wrapping the header text.
+  Widget header() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: SizedBox(),
+        ),
+        Expanded(
+          flex: 9,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(107,180,186,0.5),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    headerText(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(),
+        ),
+      ],
+    );
+  }
+
+  // Returns the header text based on the activeStep.
+  String headerText() {
+    switch (activeStep) {
+      case 1:
+        return 'Choose your Therapist';
+
+      case 2:
+        return 'Choose your Time Slot';
+
+      default:
+        return 'Book an Appointment';
+    }
+  }
+
+  // Returns the body based on the activeStep.
+  Widget body() {
+    switch (activeStep) {
+      case 1:
+        return Scaffold(
+          backgroundColor: Color.fromRGBO(240,240,235,1.0),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'List out therapists',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        );
+
+      case 2:
+        return Scaffold(
+          backgroundColor: Color.fromRGBO(240,240,235,1.0),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'List out available time slots',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        );
+
+      default:
+        return Scaffold(
+          backgroundColor: Color.fromRGBO(240,240,235,1.0),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Introductory Text',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        );
+    }
+  }
+
 }
 
 class Article extends StatelessWidget {
   const Article({ Key? key }) : super(key: key);
-
-  // final String title;
-  // final String selectedUrl;
-
-  // final Completer<WebViewController> _controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
@@ -73,26 +229,3 @@ class Article extends StatelessWidget {
     );
   }
 }
-
-// class Article extends StatefulWidget {
-//   const Article({ Key? key }) : super(key: key);
-
-//   @override
-//   _ArticleState createState() => _ArticleState();
-// }
-
-// class _ArticleState extends State<Article> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Article Title'),
-//         // centerTitle: true,
-//         backgroundColor: Color.fromRGBO(4, 98, 126,1.0),
-//         elevation: 0.0,
-//       ),
-//       backgroundColor: Color.fromRGBO(240,240,235,1.0),
-//       body: WebView(),
-//     );
-//   }
-// }
