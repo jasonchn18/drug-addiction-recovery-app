027f70e916796f259091a3b3b10661d21437c531
jasonchn18 <jasonchn18@gmail.com>
2022-03-12 05:55:59 +0800

    Display time slots list, updating available time slot, getting current user

--- lib/screens/appointment.dart
+++ lib/screens/appointment.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
@@ -18,8 +19,11 @@ class _AppointmentState extends State<Appointment> {
   int upperBound = 2;
   
   List<DocumentSnapshot> _therapists = [];
+  List<DocumentSnapshot> _time_slots = [];
   String _chosenTherapistDisplayName = "";
   String _chosenTherapistEmail = "";
+  String _chosenTimeSlotDay = "";
+  int _chosenTimeSlotTime = 0;
 
   Stream<DocumentSnapshot> snapshot =  FirebaseFirestore.instance.collection("users").doc('qDRHNNU6sxOCPNXFpvbGmdMqm3w1').snapshots();
 
@@ -29,12 +33,10 @@ class _AppointmentState extends State<Appointment> {
     });
   }
 
-  void _nextButtonAction() {
-    if (activeStep == 0) {
-      setState(() {
-        activeStep++;
-      });
-    }
+  void _startButtonAction() {
+    setState(() {
+      activeStep++;
+    });
   }
 
   @override
@@ -79,30 +81,25 @@ class _AppointmentState extends State<Appointment> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(width: 30),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 10,
+                      ),
                       Expanded(
                         child: ElevatedButton(
-                          onPressed: activeStep > 0 ? ()=>_prevButtonAction() : null,
-                          child: Text('Previous'),
+                          onPressed: activeStep == 0 ? ()=>_startButtonAction() : ()=>_prevButtonAction(),
+                          child: Text(buttonText()),
                           style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(4, 98, 126, 0.7)),
+                            backgroundColor: activeStep == 0 ?
+                              MaterialStateProperty.all<Color>(Colors.teal.shade600) : MaterialStateProperty.all<Color>(Color.fromRGBO(4, 98, 126, 0.8)),
                           ),
                         ),
+                        flex: 11,
                       ),
-                      SizedBox(width: 50),
                       Expanded(
-                        child: ElevatedButton(
-                          onPressed: activeStep != 1 ? ()=>_nextButtonAction() : null,
-                          child: Text(
-                            buttonText(),
-                          ),
-                          style: ButtonStyle(
-                            backgroundColor: activeStep < upperBound ?
-                              MaterialStateProperty.all<Color>(Color.fromRGBO(4, 98, 126, 0.7)) : MaterialStateProperty.all<Color>(Colors.teal.shade600),
-                          ),
-                        ),
+                        child: SizedBox(),
+                        flex: 10,
                       ),
-                      SizedBox(width: 30),
                     ],
                   ),
                 ),
@@ -177,20 +174,22 @@ class _AppointmentState extends State<Appointment> {
         getTherapists();
         return therapistList(_therapists);
       case 2:
-        return Scaffold(
-          backgroundColor: Color.fromRGBO(240,240,235,1.0),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Chosen therapist: $_chosenTherapistDisplayName $_chosenTherapistEmail',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ],
-          ),
-        );
+        getTimeSlots();
+        return timeSlotList(_time_slots);
+        // return Scaffold(
+        //   backgroundColor: Color.fromRGBO(240,240,235,1.0),
+        //   body: Row(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       Text(
+        //         'Chosen therapist: $_chosenTherapistDisplayName $_chosenTherapistEmail',
+        //         style: TextStyle(
+        //           fontSize: 15,
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // );
 
       default:
         return Scaffold(
@@ -220,11 +219,11 @@ class _AppointmentState extends State<Appointment> {
       return "Start";
     }
     else {
-      return "Submit";
+      return "Previous";
     }
   }
 
-  // Returns the Choose Therapist widget
+  // Returns the Therapist List widget
   Widget therapistList(List<DocumentSnapshot> therapists) {
     List<Widget> list = <Widget>[];
     for (var data in therapists) {
@@ -235,6 +234,8 @@ class _AppointmentState extends State<Appointment> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               ListTile(
+                dense: false,
+                visualDensity: VisualDensity(horizontal: -4),
                 leading: Icon(
                   Icons.person_outline_rounded,
                   color: Colors.black54,
@@ -264,25 +265,26 @@ class _AppointmentState extends State<Appointment> {
         ),
       ));
     }
-    // int num = therapists.length;
-    // return Text('la $num');
     return Scaffold(
       backgroundColor: Color.fromRGBO(240,240,235,1.0),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: list,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: list,
+          ),
         ),
       ),
     );
   }
 
-  Future<List<DocumentSnapshot>> getTherapists() async {
+  Future getTherapists() async {
     List<DocumentSnapshot> docs = [];
     
     await FirebaseFirestore.instance.collection('users')
     .where("type",isEqualTo: "T")
+    .orderBy("displayName")
     .get().then((query) {
         docs = query.docs;
     });
@@ -290,8 +292,201 @@ class _AppointmentState extends State<Appointment> {
     setState(() {
       _therapists = docs;
     });
+  }
 
-    return docs;
+  // Returns the Therapist List widget
+  Widget timeSlotList(List<DocumentSnapshot> time_slots) {
+    List<Widget> list = <Widget>[];
+
+    list.add(Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Dr. ' + _chosenTherapistDisplayName + '\'s available time slot(s):',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
+    ));
+    
+    for (var data in time_slots) {
+      // int start_time = data.get('time');  //1000
+      // int end_time = start_time + 0200;   //1200
+      // String time = start_time.toString() + " - " + end_time.toString();
+      String startTime = "";
+      String endTime = "";
+      String hour = "";
+      String minute = "";
+      String amPm = "am";
+
+      // Time formatting
+      if (data.get('time') < 1300) {
+        if (data.get('time') >= 1200) {
+          amPm = "pm";
+        }
+        startTime = (data.get('time')).toString();
+        if(startTime.length == 3) {
+          startTime = "0" + startTime;
+        }
+        if (startTime[0] == "0") {
+          hour = startTime.substring(1,2);
+        }
+        else {
+          hour = startTime.substring(0,2);
+        }
+        minute = startTime.substring(2,4);
+        startTime = hour + "." + minute + amPm;
+      }
+      if ( (data.get('time') + 0200) < 1300 ) {
+        if (data.get('time') + 0200 >= 1200) {
+          amPm = "pm";
+        }
+        endTime = (data.get('time') + 0200).toString();
+        if(endTime.length == 3) {
+          endTime = "0" + endTime;
+        }
+        if (endTime[0] == "0") {
+          hour = endTime.substring(1,2);
+        }
+        else {
+          hour = endTime.substring(0,2);
+        }
+        minute = endTime.substring(2,4);
+        endTime = hour + "." + minute + amPm;
+      }
+
+      if (data.get('time') >= 1300) {
+        if (data.get('time') >= 1200) {
+          amPm = "pm";
+        }
+        startTime = (data.get('time') - 1200).toString();
+        if(startTime.length == 3) {
+          startTime = "0" + startTime;
+        }
+        if (startTime[0] == "0") {
+          hour = startTime.substring(1,2);
+        }
+        else {
+          hour = startTime.substring(0,2);
+        }
+        minute = startTime.substring(2,4);
+        startTime = hour + "." + minute + amPm;
+      }
+      if ( (data.get('time') + 0200) >= 1300 ) {
+        if (data.get('time') + 0200 >= 1200) {
+          amPm = "pm";
+        }
+        endTime = (data.get('time') - 1200 + 0200).toString();
+        if(endTime.length == 3) {
+          endTime = "0" + endTime;
+        }
+        if (endTime[0] == "0") {
+          hour = endTime.substring(1,2);
+        }
+        else {
+          hour = endTime.substring(0,2);
+        }
+        minute = endTime.substring(2,4);
+        endTime = hour + "." + minute + amPm;
+      }
+
+      String time = startTime + " - " + endTime;
+
+      list.add(Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  data.get('day').substring(2),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    height: 1.5,
+                  ),
+                ),
+                subtitle: Text(
+                  time,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                trailing: ElevatedButton(
+                  onPressed: () {
+                    _chosenTimeSlotDay = data.get('day');
+                    _chosenTimeSlotTime = data.get('time');
+                    setTimeSlots();
+                  }, 
+                  child: Text('Select'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.teal.shade600),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(240,240,235,1.0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: list,
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Future getTimeSlots() async {
+    List<DocumentSnapshot> docs = [];
+    
+    await FirebaseFirestore.instance.collection('time_slots')
+    .where("therapist_email",isEqualTo: _chosenTherapistEmail)
+    .where("availability",isEqualTo: true)
+    .orderBy("day")
+    .orderBy("time")
+    .get().then((query) {
+      docs = query.docs;
+    });
+
+    if (mounted) {
+      setState(() {
+        _time_slots = docs;
+      });
+    }
+  }
+  
+  Future setTimeSlots() async {
+    // var db = FirebaseFirestore.instance;
+
+    // db.collection("time_slots").doc(doc.id).update({foo: "bar"});
+
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    // print(user!.email);
+
+    QuerySnapshot querySnap = await FirebaseFirestore.instance.collection('time_slots')
+      .where('therapist_email', isEqualTo: _chosenTherapistEmail)
+      .where('day', isEqualTo: _chosenTimeSlotDay)
+      .where('time', isEqualTo: _chosenTimeSlotTime)
+      .get();
+    QueryDocumentSnapshot doc = querySnap.docs[0];  // Assumption: the query returns only one document, THE doc you are looking for.
+    DocumentReference docRef = doc.reference;
+    await docRef.update({
+      'availability': false,
+      'booked_by': user!.email,
+    });
   }
   
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp_app/services/auth.dart';
 import 'package:fyp_app/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:fyp_app/user_list.dart';
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+final User? user = auth.currentUser;
+String _displayName = "";
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -28,49 +33,91 @@ class _HomeState extends State<Home> {
           child: Column(
             children: <Widget>[
               UserList(),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton.icon(
-                        onPressed: () {
-                          // Navigator.pushNamed(context, '/home');
-                        }, 
-                        icon: Icon(
-                          Icons.home_rounded,
-                          color: Colors.black,
-                        ),
-                        label: Text(
-                          'Home screen',
-                          style: TextStyle(
-                            color: Colors.black,
+              Column(
+                children: [
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: TextButton.icon(
+                            onPressed: () {
+                              // Navigator.pushNamed(context, '/home');
+                            }, 
+                            icon: Icon(
+                              Icons.home_rounded,
+                              color: Colors.black,
+                            ),
+                            label: Text(
+                              'Home screen',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right:8.0),
-                        child: TextButton.icon(
-                          onPressed: () async {
-                            await _auth.signOut();
-                          }, 
-                          icon: Icon(Icons.person), 
-                          label: Text('Logout')
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right:8.0),
+                            child: TextButton.icon(
+                              onPressed: () async {
+                                await _auth.signOut();
+                              }, 
+                              icon: Icon(Icons.person), 
+                              label: Text('Logout')
+                            ),
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: displayUserDisplayName(),
                     ),
                   ),
                 ],
               ),
+              
             ],
           ),
         ),
       ),
     );
   }
+
+  Future getUserDisplayName() async {
+    // String displayName = "";
+    // var db = FirebaseFirestore.instance;
+    // db.collection('users').doc(user!.uid).get();
+    // var docRef = db.collection("users").doc(user!.uid);
+    await FirebaseFirestore.instance.collection('users')
+      .doc(user!.uid)
+      .get().then((query) {
+        _displayName = query.get('displayName');
+      });
+
+    // docRef.get().then((doc) => {
+    //   if (doc.exists) {
+    //     displayName = doc.get('displayName')
+    //   }
+    // });
+    // return displayName;
+  }
+
+  Widget displayUserDisplayName() {
+    getUserDisplayName();
+    return Text(
+      'Hi ' + _displayName,
+      style: TextStyle(
+        fontSize: 20,
+      ),
+    );
+  }
+
 }
